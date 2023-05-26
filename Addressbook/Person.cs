47f987fe664eb7164d0bff43e17c75b91183b6bb
Sławek Witek <slawekwitek@icloud.com>
2023-05-26 15:57:09 +0200

    Bug fixes

--- Addressbook/Person.cs
+++ Addressbook/Person.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 namespace Addressbook
 {
-	public class Person
-	{
+    public class Person
+    {
         public int Id { get; set; }
-		public string FirstName { get; set; }
-		public string LastName { get; set; }
-		public string Email { get; set; }
-		public string PhoneNumber { get; set; }
-	}
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
 }
 
